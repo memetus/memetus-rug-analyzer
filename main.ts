87350addfa10df3242bbd6befd6acddd03fb1e7e
resister-boy <hassanpumped17@gmail.com>
2025-01-06 17:25:27 +0900

    feat: add entry file

--- main.ts
+++ main.ts
@@ -1,7 +1,30 @@
-import { logger } from '@/src/utils/logger';
+import { HolderChecker } from "@/src/module/checker/holderChecker";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { nodeEndpoint } from "@/src/constant/solana-cluster-url";
 
-const main = () => {
-  logger();
-};
+export default class MemetusRugChecker {
+  connection: Connection;
+  holderChecker: HolderChecker;
+
+  constructor() {
+    this.connection = new Connection(nodeEndpoint["quickNode"], "confirmed");
+
+    console.log(this.connection);
+    this.holderChecker = new HolderChecker({
+      connection: this.connection,
+      tokenAddress: new PublicKey(
+        "7Uuzh9JwqF8z3u6MWpQuQJbpD1u46xPDY6PGjwfwTh4o"
+      ),
+      creator: new PublicKey("3DBzxVrhNaFTLAcdP9kkiZxFTtBoMb1LYww6n1NUYdzx"),
+    });
+  }
+}
+
+function main() {
+  const memetusRugChecker = new MemetusRugChecker();
+  memetusRugChecker.holderChecker.check().then((response) => {
+    console.log(response);
+  });
+}
 
 main();
