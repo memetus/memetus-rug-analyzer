f1ffc5c4a365a15a473312e3b67c3ea6ac69e698
resister-boy <hassanpumped17@gmail.com>
2025-01-19 01:47:35 +0900

    comment: add base checker description

--- src/model/baseChecker.ts
+++ src/model/baseChecker.ts
@@ -1,3 +1,8 @@
 interface IBaseChecker {}
 
+/**
+ * Base class for all checkers.
+ * @implements {IBaseChecker}
+ * @description Every checker class should extend this class.
+ */
 export class BaseChecker implements IBaseChecker {}
