78439caba94381a6e2743e68d723aa415a2239cd
resister-boy <hassanpumped17@gmail.com>
2025-01-18 18:49:49 +0900

    reset project

--- src/types/checker/checker.d.ts
+++
@@ -1,3 +0,0 @@
-export type CheckerResponse = {
-  score: number;
-};
--- src/types/checker/holderChecker.d.ts
+++
@@ -1,7 +0,0 @@
-import { PublicKey } from "@solana/web3.js";
-
-export type HolderCheckData = {
-  address: PublicKey;
-  amount: number;
-  percentage: number;
-};
