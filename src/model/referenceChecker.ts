4de386ea5fe5d9e8acb82d2c53a1518fd9d7fd6b
resister-boy <hassanpumped17@gmail.com>
2025-01-19 01:49:40 +0900

    comment: add reference checker description

--- src/model/referenceChecker.ts
+++ src/model/referenceChecker.ts
@@ -1,5 +1,13 @@
 import { BaseChecker } from "@/src/model/baseChecker";
 
-interface ITeamChecker {}
+interface IReferenceChecker {}
 
-export class TeamChecker extends BaseChecker implements ITeamChecker {}
+/**
+ * ReferenceChecker
+ * @implements {IReferenceChecker}
+ * @description This class is responsible for checking the reference of a project.
+ * It should check the project's reference and verify it. It include website, whitepaper, github and twitter curretly.
+ */
+export class IReferecneChecker
+  extends BaseChecker
+  implements IReferenceChecker {}
