176d9762e45c476adc1494a40a188adfbed06ace
resister-boy <hassanpumped17@gmail.com>
2025-01-19 01:49:07 +0900

    comment: add metadata checker description

--- src/model/metadataChecker.ts
+++ src/model/metadataChecker.ts
@@ -2,4 +2,11 @@ import { BaseChecker } from "@/src/model/baseChecker";
 
 interface IMetadataChecker {}
 
+/**
+ * MetadataChecker
+ * @implements {IMetadataChecker}
+ * @description This class is responsible for checking the metadata of a project.
+ * It should check the name, symbol, description, mutability, mintability, burnability, freezability and total supply of the project.
+ */
+
 export class MetadataChecker extends BaseChecker implements IMetadataChecker {}
