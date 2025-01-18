0ae66dd108eeb21f2591eb1346512c8c5a50ab93
resister-boy <hassanpumped17@gmail.com>
2025-01-19 01:48:37 +0900

    comment: add liquidity checker description

--- src/model/liquidityChecker.ts
+++ src/model/liquidityChecker.ts
@@ -2,6 +2,12 @@ import { BaseChecker } from "@/src/model/baseChecker";
 
 interface ILiquidityChecker {}
 
+/**
+ * LiquidityChecker
+ * @implements {ILiquidityChecker}
+ * @description This class is responsible for checking the liquidity stability of a project.
+ * It should check if the project has a liquidity pool and if it is locked.
+ */
 export class LiquidityChecker
   extends BaseChecker
   implements ILiquidityChecker {}
