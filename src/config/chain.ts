4a247d33d5fd6b955c332e8e2102ed2eecdbfe53
resister-boy <hassanpumped17@gmail.com>
2025-01-20 13:48:21 +0900

    feat: add umi creator

--- src/config/chain.ts
+++ src/config/chain.ts
@@ -1,5 +1,6 @@
 import { Connection } from "@solana/web3.js";
 import { nodeEndpoint } from "@/src/config/rpc";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 
 /**
  * TODO: should update to use the connection pool
@@ -15,7 +16,7 @@ export const createConnection = () => {
 
   for (const endpoint of endpoints) {
     try {
-      connection = new Connection(endpoint);
+      connection = new Connection(endpoint, "confirmed");
       return connection;
     } catch (e) {
       console.log(`Failed to connect to ${endpoint}`);
@@ -27,4 +28,20 @@ export const createConnection = () => {
   }
 
   return connection;
+};
+
+export const createUmiEndpoint = () => {
+  const endpoints = [
+    nodeEndpoint["instantNode"],
+    nodeEndpoint["helius"],
+    nodeEndpoint["quickNode"],
+  ];
+
+  for (const endpoint of endpoints) {
+    try {
+      return createUmi(endpoint);
+    } catch (e) {
+      console.log(`Failed to connect to ${endpoint}`);
+    }
+  }
 };
