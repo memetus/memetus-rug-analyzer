27f47c45748ae9c3487c3dfcfc5698c2495f3898
resister-boy <hassanpumped17@gmail.com>
2025-01-20 16:52:54 +0900

    feat: add helius module

---
+++ src/module/heliusModule.ts
@@ -0,0 +1,38 @@
+import { Helius } from "helius-sdk";
+import { getEnv } from "../utils/env";
+import axios from "axios";
+
+interface IHeliusModule {}
+
+export class HeliusModule implements IHeliusModule {
+  client: Helius;
+  apiKey: string;
+  endPointUrl: string;
+
+  constructor() {
+    this.apiKey = getEnv("HELIUS_API_KEY");
+    this.endPointUrl = "https://mainnet.helius-rpc.com";
+    this.client = new Helius(this.apiKey);
+  }
+
+  async getMetadata(address: string) {
+    try {
+      const res = await axios.post(
+        `${this.endPointUrl}?api-key=${this.apiKey}`,
+        {
+          jsonrpc: "2.0",
+          id: "test",
+          method: "getAsset",
+          params: {
+            id: address,
+          },
+        }
+      );
+
+      return res.data;
+    } catch (error) {
+      console.error(error);
+      throw new Error("Failed to get metadata");
+    }
+  }
+}
