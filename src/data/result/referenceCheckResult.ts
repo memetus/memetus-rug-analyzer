297095bc2297f01bd6acdedec41d669739c01e1f
resister-boy <hassanpumped17@gmail.com>
2025-01-20 22:44:19 +0900

    interface: add reference check result interface

---
+++ src/data/result/referenceCheckResult.ts
@@ -0,0 +1,19 @@
+import { BaseCheckResult } from "./baseCheckResult";
+
+interface IReferenceCheckResult {}
+
+/**
+ * ReferenceCheckResult
+ * @implements {IReferenceCheckResult}
+ * @description This class is responsible for checking the reference of a project.
+ * It should check if the project has a reference.
+ */
+
+export class ReferenceCheckResult
+  extends BaseCheckResult
+  implements IReferenceCheckResult
+{
+  constructor(score: number) {
+    super(score);
+  }
+}
