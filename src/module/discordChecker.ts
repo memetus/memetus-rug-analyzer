47a968e7a63bef9532ea96f1f75a082528fbf045
resister-boy <hassanpumped17@gmail.com>
2025-01-20 18:46:21 +0900

    feat: add discord checker

---
+++ src/module/discordChecker.ts
@@ -0,0 +1,16 @@
+interface IDiscordChecker {}
+
+/**
+ * DiscordChecker
+ * @implements {IDiscordChecker}
+ * @description This class is responsible for checking the discord of a project.
+ * It should check if the project has a discord account.
+ */
+
+export class DiscordChecker implements IDiscordChecker {
+  channel: string;
+
+  constructor(channel: string) {
+    this.channel = channel;
+  }
+}
