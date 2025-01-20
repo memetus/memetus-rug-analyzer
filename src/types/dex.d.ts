24601a9aca571a4f148f3eb297ebb966b5d9fb31
resister-boy <hassanpumped17@gmail.com>
2025-01-20 15:08:11 +0900

    types: declare dexscreener response shape

--- src/types/dex.d.ts
+++ src/types/dex.d.ts
@@ -6,3 +6,47 @@ export type DexSupplyShape = {
   name: DexType;
   amount: number;
 };
+
+export type DexScreenerResponseShape = {
+  chainId: string;
+  dexId: string;
+  url: string;
+  pairAddress: string;
+  baseToken: {
+    address: string;
+    name: string;
+    symbol: string;
+  };
+  quoteToken: {
+    address: string;
+    name: string;
+    symbol: string;
+  };
+  priceNative: string;
+  priceUsd: string;
+  liquidity: {
+    usd: number;
+    base: number;
+    quote: number;
+  };
+  fdv: number;
+  marketCap: number;
+  pairCreatedAt: number;
+  info: {
+    imageUrl: string;
+    header: string;
+    openGraph: string;
+    websites: DexSceenerWebSiteShape[];
+    socials: DexScreenerSocialShape[];
+  };
+};
+
+export type DexSceenerWebSiteShape = {
+  label: string;
+  url: string;
+};
+
+export type DexScreenerSocialShape = {
+  type: string;
+  url: string;
+};
