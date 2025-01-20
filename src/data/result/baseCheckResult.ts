b4b23ad974971e458465fe2b1807e295ea1f3764
resister-boy <hassanpumped17@gmail.com>
2025-01-20 22:41:51 +0900

    interface: add base check result interface

---
+++ src/data/result/baseCheckResult.ts
@@ -0,0 +1,22 @@
+interface IBaseCheckResult {
+  score: number;
+}
+
+/**
+ * BaseCheckResult
+ * @implements {IBaseCheckResult}
+ * @description This class is responsible for aggregate all checker in single module.
+ * It should contain twitterChecker, discordChecker, telegramChecker.
+ */
+
+export class BaseCheckResult implements IBaseCheckResult {
+  score: number;
+
+  constructor(score: number) {
+    this.score = score;
+  }
+
+  public async getScore() {
+    return this.score;
+  }
+}
