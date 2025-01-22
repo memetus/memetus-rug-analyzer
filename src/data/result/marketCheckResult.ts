1ef937022280fed4e6b59a3df6f8f96a6dbecf0c
resister-boy <hassanpumped17@gmail.com>
2025-01-22 13:11:13 +0900

    feat: update market check result

--- src/data/result/marketCheckResult.ts
+++ src/data/result/marketCheckResult.ts
@@ -1,4 +1,6 @@
-import { BaseCheckResult } from "./baseCheckResult";
+import { Txn, Volume, DataChangeShpae } from "@/src/types/market";
+import { BaseCheckResult } from "@/src/data/result/baseCheckResult";
+import { BaseGrowth } from "@/src/data/baseGrowth";
 
 interface IMarketCheckResult {}
 
@@ -13,7 +15,57 @@ export class MarketCheckResult
   extends BaseCheckResult
   implements IMarketCheckResult
 {
+  priceNative: number;
+  priceUsd: number;
+  m5Txns: Txn;
+  h1Txns: Txn;
+  h6Txns: Txn;
+  h24Txns: Txn;
+  volume: Volume;
+  volumeChange: DataChangeShpae;
+  priceChange: DataChangeShpae;
+  marketCap: number;
+  fdv: number;
+  marketGrowth: BaseGrowth;
+
   constructor(score: number) {
     super(score);
+    this.priceNative = 0;
+    this.priceUsd = 0;
+    this.m5Txns = {
+      buy: 0,
+      sell: 0,
+    };
+    this.h1Txns = {
+      buy: 0,
+      sell: 0,
+    };
+    this.h6Txns = {
+      buy: 0,
+      sell: 0,
+    };
+    this.h24Txns = {
+      buy: 0,
+      sell: 0,
+    };
+    this.volume = {
+      m5: 0,
+      h1: 0,
+      h6: 0,
+      h24: 0,
+    };
+    this.volumeChange = {
+      h1: 0,
+      h6: 0,
+      h24: 0,
+    };
+    this.priceChange = {
+      h1: 0,
+      h6: 0,
+      h24: 0,
+    };
+    this.marketCap = 0;
+    this.fdv = 0;
+    this.marketGrowth = new BaseGrowth();
   }
 }
