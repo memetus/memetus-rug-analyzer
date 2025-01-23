0eb4e138ee1c18d71cbba2e1d715fd7636ad91b3
resister-boy <hassanpumped17@gmail.com>
2025-01-23 14:03:35 +0900

    feat: update base community module

--- src/data/baseCommunity.ts
+++ src/data/baseCommunity.ts
@@ -1,7 +1,11 @@
 import { TwitterChecker } from "@/src/module/twitterChecker";
+import { DiscordChecker } from "@/src/module/discordChecker";
+import { TelegramChecker } from "@/src/module/telegramChecker";
 
 export class BaseCommunity {
   twitter!: TwitterChecker;
+  discord!: DiscordChecker;
+  telegram!: TelegramChecker;
 
   constructor() {}
 }
