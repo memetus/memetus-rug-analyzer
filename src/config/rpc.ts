2ad37c1373a7b8e94144c52dd7fb8a6d789a7ad8
resister-boy <hassanpumped17@gmail.com>
2025-01-23 19:36:22 +0900

    config: update rpc config

--- src/config/rpc.ts
+++ src/config/rpc.ts
@@ -4,7 +4,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const nodeEndpoint: Record<string, string> = {
-  quickNode: `https://maximum-muddy-dinghy.solana-mainnet.quiknode.pro/${process.env.QUICKNODE_API_KEY}`,
-  instantNode: `https://solana-api.instantnodes.io/token-${process.env.INSTANTNODE_API_KEY}`,
   helius: `https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY}`,
 };
