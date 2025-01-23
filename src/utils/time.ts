179226bf3f01c890f0f7be41df9e087bc86f6db7
resister-boy <hassanpumped17@gmail.com>
2025-01-23 14:04:20 +0900

    feat: update time diff utils

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -2,12 +2,20 @@ export const getTimezone = () => {
   return Intl.DateTimeFormat().resolvedOptions().timeZone;
 };
 
-export const timeDifference = (start: Date, end: Date): string => {
+export const timeDifference = (
+  start: Date,
+  end: Date,
+  type?: "d" | "h" | "m" | "s"
+): string => {
   const diffMs = Math.abs(end.getTime() - start.getTime());
   const diffSec = Math.floor(diffMs / 1000);
   const hours = Math.floor(diffSec / 3600);
   const minutes = Math.floor((diffSec % 3600) / 60);
   const seconds = diffSec % 60;
+
+  if (type === "h") return hours.toString();
+  if (type === "m") return minutes.toString();
+  if (type === "s") return seconds.toString();
   return `${hours}h ${minutes}m ${seconds}s`;
 };
 
