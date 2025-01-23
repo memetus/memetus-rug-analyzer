39bb24c9842ef614018d148f832f2bf21793ef18
resister-boy <hassanpumped17@gmail.com>
2025-01-23 15:34:45 +0900

    types: declare holder check result data

--- src/types/holder.d.ts
+++ src/types/holder.d.ts
@@ -1,5 +1,28 @@
+import { DexSupplyShape } from "@/src/types/dex";
+
 export type TopHolderSupplyShape = {
   address: string;
   amount: number;
   percentage: number;
+};
+
+export type HolderData = {
+  dexPercentage: number;
+  top10: {
+    percentage: number;
+    holders: TopHolderSupplyShape[];
+  };
+  top20: {
+    percentage: number;
+    holders: TopHolderSupplyShape[];
+  };
+  top50: {
+    percentage: number;
+    holders: TopHolderSupplyShape[];
+  };
+  creatorBalance: number;
+  creatorPercentage: number;
+  dexSupplys: DexSupplyShape[];
+  totalHolderCount: number;
+  topHolderValidation: { address: string; isValid: boolean }[];
 };
