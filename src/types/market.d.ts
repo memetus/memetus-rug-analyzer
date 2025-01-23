75b7f5a07549c7876be3d8703dd27a7b37ea883b
resister-boy <hassanpumped17@gmail.com>
2025-01-23 18:07:26 +0900

    types: declare market data types

--- src/types/market.d.ts
+++ src/types/market.d.ts
@@ -3,3 +3,18 @@ export type Txn = { buy: number; sell: number };
 export type Volume = { m5: number; h1: number; h6: number; h24: number };
 
 export type DataChangeShpae = { h1: number; h6: number; h24: number };
+
+export type MarketData = {
+  priceNative: number;
+  priceUsd: number;
+  m5Txns: Txn;
+  h1Txns: Txn;
+  h6Txns: Txn;
+  h24Txns: Txn;
+  volume: Volume;
+  volumeChange: DataChangeShpae;
+  priceChange: DataChangeShpae;
+  marketCap: number;
+  fdv: number;
+  marketGrowth: BaseGrowth;
+};
