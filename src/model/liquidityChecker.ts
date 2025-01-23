419a7b1063738100d88d4679accbd8c9e2807a06
resister-boy <hassanpumped17@gmail.com>
2025-01-23 17:12:07 +0900

    feat: update liquidity check result

--- src/model/liquidityChecker.ts
+++ src/model/liquidityChecker.ts
@@ -3,6 +3,8 @@ import { Connection, ParsedAccountData, PublicKey } from "@solana/web3.js";
 import { DexScreenerGetter } from "@/src/module/dexScreenerGetter";
 import { DexScreenerResponseShape } from "@/src/types/dex";
 import { LIQUIDITY_STATE_LAYOUT_V4 } from "@raydium-io/raydium-sdk";
+import { LiquidityCheckResult } from "@/src/data/result/liquidityCheckResult";
+import { LiquidityData } from "../types/liquidity";
 
 interface ILiquidityChecker {}
 
@@ -22,32 +24,75 @@ export class LiquidityChecker extends BaseChecker implements ILiquidityChecker {
     this.connection = connection;
   }
 
-  public async check() {}
+  public async check() {
+    const checkResult = new LiquidityCheckResult();
+    const liquidityData = await this.getPoolInfo();
+    let totalLockedAmount = 0;
+    let totalLockedPercentage = 0;
+    const marketCap = liquidityData.dexList[0]?.marketCap;
+
+    const lpLockedInfoList = await Promise.all(
+      liquidityData.dexList.slice(0, 5).map(async (lp) => {
+        if (lp) {
+          const lockedInfo = await this.checkTokenLocked(lp.pair);
+          totalLockedAmount += lockedInfo?.burnAmount ?? 0;
+          totalLockedPercentage += lockedInfo?.burnPercentage ?? 0;
+
+          return lockedInfo;
+        }
+      })
+    );
+
+    const data: LiquidityData = {
+      totalLiquidity: liquidityData.totalDexLiquidity,
+      totalLpCount: liquidityData.dexList.length,
+      lpList: liquidityData.dexList,
+      lpLockedInfoList: lpLockedInfoList,
+      totalLockedLiquidity: totalLockedAmount,
+      totalLockedLiquidityPercentage: totalLockedPercentage,
+      marketCap,
+    };
+
+    return checkResult.setData({ ...data }).then(async () => {
+      return await checkResult.getScore();
+    });
+  }
 
   public async getPoolInfo() {
-    const dexList: DexScreenerResponseShape[] = await this.getDexPoolList();
+    const dexList: DexScreenerResponseShape[] =
+      await this.getDexPoolList().then((list: DexScreenerResponseShape[]) => {
+        return list.sort((a, b) => b.marketCap - a.marketCap);
+      });
     const pairList: string[] = [];
-    const totalLiquidity = dexList.reduce((acc, dex) => {
-      return dex.liquidity.usd + acc;
-    }, 0);
+    let totalLiquidity = 0;
 
-    const result = dexList.map((dex) => {
+    dexList.forEach((dex) => {
+      if (dex.liquidity && "usd" in dex.liquidity) {
+        totalLiquidity += dex.liquidity.usd;
+      }
+    });
+
+    const result = dexList?.map((dex) => {
       if (!pairList.includes(dex.quoteToken.address)) {
         pairList.push(dex.quoteToken.address);
       }
-      return {
-        name: dex.dexId,
-        pair: dex.pairAddress,
-        liquidity: dex.liquidity.usd,
-        liquidityPercentage: (dex.liquidity.usd / totalLiquidity) * 100,
-      };
+      if (dex.liquidity && "usd" in dex.liquidity) {
+        totalLiquidity += dex.liquidity.usd;
+        return {
+          name: dex.dexId,
+          pair: dex.pairAddress,
+          marketCap: dex.marketCap,
+          liquidity: dex.liquidity.usd,
+          lpPercentage: (dex.liquidity.usd / totalLiquidity) * 100,
+        };
+      }
     });
 
     return {
       pairType: pairList,
       pairTypeCount: pairList.length,
       totalDexLiquidity: totalLiquidity,
-      dexList: result,
+      dexList: result.filter((v) => v && v),
     };
   }
 
@@ -65,6 +110,15 @@ export class LiquidityChecker extends BaseChecker implements ILiquidityChecker {
         new PublicKey(lpMint)
       );
 
+      if (!accInfo || !accInfo.value) {
+        return {
+          supply: 0,
+          burnAmount: 0,
+          burnPercentage: 0,
+          isLocked: false,
+          pair,
+        };
+      }
       const mintInfo = (accInfo?.value?.data as ParsedAccountData).parsed?.info;
 
       lpReserve = lpReserve / Math.pow(10, mintInfo?.decimals);
