e76a9b7d795f8a634ef2a681794031d38c6471a0
resister-boy <hassanpumped17@gmail.com>
2025-01-26 11:23:21 +0900

    feat: add memetus type decision agent

--- src/model/memetus/agent.ts
+++ src/model/memetus/agent.ts
@@ -1,5 +1,20 @@
-import { createModelWithTool } from "@/src/config/agent";
+import {
+  createAgentState,
+  createModel,
+  createModelWithTool,
+} from "@/src/config/agent";
+import { logger } from "@/src/config/log";
+import { createSearchTool } from "@/src/config/tool";
+import { SYSTEM_PROMPT } from "@/src/constant/prompt";
+import { SearchTool } from "@/src/types/tool";
 import { getEnv } from "@/src/utils/env";
+import {
+  AIMessage,
+  BaseMessage,
+  SystemMessage,
+} from "@langchain/core/messages";
+import { END, Messages, START, StateGraph } from "@langchain/langgraph";
+import { StructuredToolInterface, Tool } from "@langchain/core/tools";
 
 interface IMemetusRugAgent {}
 
@@ -12,9 +27,54 @@ interface IMemetusRugAgent {}
  */
 export class MemetusRugAgent implements IMemetusRugAgent {
   model: ReturnType<typeof createModelWithTool>;
+  tools: StructuredToolInterface[];
+  state: ReturnType<typeof createAgentState>;
+  workflow: StateGraph<BaseMessage[], Messages>;
+  graph!: any;
 
-  constructor() {
+  constructor(tool: SearchTool) {
     const modelId = getEnv("MODEL_ID");
-    this.model = createModelWithTool({ modelId, tools: [] });
+    this.tools = [tool];
+    this.state = createAgentState();
+    this.model = createModelWithTool({ modelId, tools: this.tools });
+    this.workflow = new StateGraph(this.state);
+  }
+
+  static async init() {
+    const searchTool = await createSearchTool();
+    return new MemetusRugAgent(searchTool);
+  }
+
+  public compile() {
+    this.tools.forEach((tool) => {
+      this.workflow.addNode("search", tool);
+    });
+    this.workflow.addNode("run", this.run.bind(this));
+
+    this.workflow.addEdge(START, "search" as any);
+    this.workflow.addEdge("search" as any, "run" as any);
+    this.workflow.addEdge("run" as any, END);
+
+    this.graph = this.workflow.compile();
+
+    return this.graph;
+  }
+
+  public async run(state: typeof this.state.State) {
+    try {
+      const messages = [new SystemMessage(SYSTEM_PROMPT), ...state.messages];
+      const modelId = getEnv("MODEL_ID");
+
+      const response = await createModel({ modelId }).invoke(messages);
+      console.log("response", response);
+
+      return {
+        messages: [new AIMessage(response.content.toString())],
+      };
+    } catch (error) {
+      console.error(error);
+      logger.error(error);
+      throw new Error(`Failed to search ${error as string}`);
+    }
   }
 }
