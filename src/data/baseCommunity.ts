aaf782d2f4f7b6e5d2796e4f43171d08d285e539
resister-boy <hassanpumped17@gmail.com>
2025-01-26 11:21:44 +0900

    feat: update baes community

--- src/data/baseCommunity.ts
+++ src/data/baseCommunity.ts
@@ -3,9 +3,22 @@ import { DiscordChecker } from "@/src/module/discordChecker";
 import { TelegramChecker } from "@/src/module/telegramChecker";
 
 export class BaseCommunity {
-  twitter!: TwitterChecker;
-  discord!: DiscordChecker;
-  telegram!: TelegramChecker;
+  twitter: {
+    handle: string | undefined;
+    checker: TwitterChecker | null;
+  };
+  discord: {
+    handle: string | undefined;
+    checker: DiscordChecker | null;
+  };
+  telegram: {
+    handle: string | undefined;
+    checker: TelegramChecker | null;
+  };
 
-  constructor() {}
+  constructor() {
+    this.twitter = { handle: undefined, checker: null };
+    this.discord = { handle: undefined, checker: null };
+    this.telegram = { handle: undefined, checker: null };
+  }
 }
