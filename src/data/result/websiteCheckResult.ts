960082b8ac31aa589921cdb9d940ed989f27eb00
resister-boy <hassanpumped17@gmail.com>
2025-01-26 11:20:40 +0900

    feat: update website check result

--- src/data/result/websiteCheckResult.ts
+++ src/data/result/websiteCheckResult.ts
@@ -1,4 +1,5 @@
-import { BaseCheckResult } from "./baseCheckResult";
+import { BaseCheckResult } from "@/src/data/result/baseCheckResult";
+import { WebsiteData, WebsiteShape } from "@/src/types/website";
 
 interface IWebsiteCheckResult {}
 
@@ -14,23 +15,49 @@ export class WebsiteCheckResult
   implements IWebsiteCheckResult
 {
   total: number;
-  urls: {
-    url: string;
-    isValid: boolean;
-    name: string;
-    description: string;
-    keywords: string;
-    canonical: string;
-    autor: string;
-    generator: string;
-    ipAddress: string;
-    location: string;
-    company: string;
-  }[];
+  urls: WebsiteShape[];
   constructor() {
     const score = 0;
     super(score);
     this.total = 0;
     this.urls = [];
+  }
+
+  public async setData({ data }: { data: WebsiteData }) {
+    this.total = data.total;
+    this.urls = data.urls;
+  }
+
+  public async getScore() {
+    if (this.total === 0) {
+      return -100;
+    } else {
+      this.score += this.total * 10;
+    }
+
+    for (const url of this.urls) {
+      if (url.name !== "") {
+        this.score += 10;
+      }
+      if (url.description !== "") {
+        this.score += 10;
+      }
+      if (url.author !== "") {
+        this.score += 10;
+      }
+      if (url.keywords !== "") {
+        this.score += 10;
+      }
+      if (url.url !== "") {
+        this.score += 10;
+      }
+    }
+    if (this.score > 100) {
+      return 100;
+    } else if (this.score < -100) {
+      return -100;
+    }
+
+    return this.score;
   }
 }
