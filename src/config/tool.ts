a08c0ceee45f618b1eec7adcbfe91fa228ef1860
resister-boy <hassanpumped17@gmail.com>
2025-01-26 11:02:38 +0900

    config: update tool types

---
+++ src/config/tool.ts
@@ -0,0 +1,70 @@
+import { DynamicStructuredTool, tool } from "@langchain/core/tools";
+import { z } from "zod";
+import { tavily } from "@tavily/core";
+import { getEnv } from "@/src/utils/env";
+import { BaseMessage, HumanMessage } from "@langchain/core/messages";
+
+export const getSearchTool = new DynamicStructuredTool({
+  name: "search-tool",
+  description: "Search tool for langgraph tool",
+  schema: z.object({
+    // content: z.string(),
+    messages: z.array(z.string()),
+  }),
+  func: async ({ messages }: { messages: string[] }) => {
+    const searchResult = await extractSearch({ messages });
+
+    return searchResult;
+  },
+});
+
+/**
+ * @description create search url tool for langgraph tool
+ * @returns langgrah tool for multi agent interaction
+ */
+export const createSearchTool = async () => {
+  const customTool = tool(
+    async ({ messages }: { messages: string[] }) => {
+      const searchResult = await extractSearch({ messages });
+
+      return searchResult;
+    },
+    {
+      ...getSearchTool,
+    }
+  );
+
+  return customTool;
+};
+
+export const createTavilyClient = () => {
+  const apiKey = getEnv("TAVILY_API_KEY");
+  const tavilyClient = tavily({
+    apiKey,
+  });
+
+  return tavilyClient;
+};
+
+/**
+ *
+ * @param urls
+ * @description use tavily to extract information from urls
+ * @returns searchResult
+ */
+export const extractSearch = async ({ messages }: { messages: string[] }) => {
+  try {
+    const tavilyClient = createTavilyClient();
+
+    const serachResult = await tavilyClient.extract(messages);
+    const joinedResult = serachResult.results.map((result) => {
+      return new HumanMessage(result.rawContent);
+    });
+
+    return {
+      messages: joinedResult,
+    };
+  } catch (error) {
+    throw new Error(`Failed to search urls ${error as string}`);
+  }
+};
